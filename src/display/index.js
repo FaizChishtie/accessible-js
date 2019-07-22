757264baf9f55d0638585e99fcec109003c9cb3e
Siddharth Rao <t-sirao@microsoft.com>
2019-07-22 16:02:45 -0700

    Added test for setting cookie

--- src/display/index.js
+++ src/display/index.js
@@ -5,10 +5,11 @@ const render = (something) => {
 }
 
 //sets accessiblity options such as contrast, font-size, etc into a cookie
-exports.setOptions = (formData) => {
+const setOptions = (formData) => {
     let options = {screenReader: formData.screenReader, contrast: formData.contrast, fontSize: formData.fontSize};
-    let jsonOptions = JSON.stringify(options);
-    document.cookie = "options =  " + options + ";path=/";
+    let JsonOptions = JSON.stringify(options);
+    document.cookie = JsonOptions;
+    return document.cookie; 
 }
 module.exports = {
     render,
