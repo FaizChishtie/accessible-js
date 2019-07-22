3b97c011e1bd8ddeb8f642d72293f316459e9bb7
Faizaan Chishtie <31967593+FaizChishtie@users.noreply.github.com>
2019-07-22 16:20:38 -0700

    update test name

--- test/display/index.js
+++ test/display/index.js
@@ -13,7 +13,7 @@ describe('test display functions', () => {
         const results = display.render('something');
         expect(results).to.equal('something');
     });
-    it('should return same object', () => {
+    it('should set display options', () => {
         const results = display.setOptions({screenReader: true, contrast: 'high', fontSize: 16});
         let parsed = JSON.parse(results);
         expect(parsed.screenReader+";"+parsed.contrast+";"+parsed.fontSize).to.equal('true;high;16');
