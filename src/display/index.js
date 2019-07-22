c52c60256bd98cdf47b9eb25369712ca3c6c58fb
Siddharth Rao <t-sirao@microsoft.com>
2019-07-22 15:18:44 -0700

    added set options function

--- src/display/index.js
+++ src/display/index.js
@@ -4,6 +4,13 @@ const render = (something) => {
     return something;
 }
 
+//sets accessiblity options such as contrast, font-size, etc into a cookie
+exports.setOptions = (formData) => {
+    let options = {screenReader: formData.screenReader, contrast: formData.contrast, fontSize: formData.fontSize};
+    let jsonOptions = JSON.stringify(options);
+    document.cookie = "options =  " + options + ";path=/";
+}
 module.exports = {
     render,
+    setOptions
 };
