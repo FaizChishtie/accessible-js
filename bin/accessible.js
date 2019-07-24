6d04fa8b72761b56be71ee668baa67554eee68c3
Faizaan Chishtie <faizchishtie@gmail.com>
2019-07-24 14:56:04 -0700

    add help

--- bin/accessible.js
+++ bin/accessible.js
@@ -8,6 +8,11 @@ if (argv["install"]) {
     console.log('Installation complete...\nMake sure to initialize accessible-js by importing it and calling apply stamp!');
 }
 
+if (argv["help"]) {
+    console.log("\t--install\t\tInstalls module to a folder");
+    console.log("\t--install\t\Configures module .accessible file within a folder");
+}
+
 if (argv["config"]) {
     installation.configuration.start();
 }
